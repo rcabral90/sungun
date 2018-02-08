c1a2d6384b4cae0fa0b9472927716aa49bc3495f
Robert Cabral <rcabral@payoff.com>
2018-02-07 20:31:33 -0800

    Pushing Project Files.

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const json = require('../resources/mock_data.json')
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: json.data });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
