ba3c7c2adf92784b4f4f940a7b8c1e4cc9e9475e
Mark Real <marki809@hotmail.com>
2018-03-28 21:23:04 -0700

    added blahblah blah and then like did you uh add the quotes? to that message? then end quote, then enter

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const json = require('./resources/mock_data.json')
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://root@45.76.175.175:27017/";
+var url = "mongodb://sungun:1burger@207.148.14.210:27017/";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,12 +10,32 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: json.data });
 });
 
-app.get('/api/getAllWorkouts', (req, res) => {
+app.get('/api/muscle', (req, res) => {
 	MongoClient.connect(url, function(err, db) {
-  		if (err) throw err;
-		var dbo = db.db("SunGun");
-		dbo.collection('Workouts').find().toArray(function(err, items){
-			res.send({ express: items});
+  		if (err) console.log(err);
+		var dbo = db.db("sungun");
+		var data = dbo.collection('tblMuscles').find().toArray(function(err, items){
+			res.send(items);
+		});
+	});
+});
+
+app.get('/api/excercise', (req, res) => {
+	MongoClient.connect(url, function(err, db) {
+  		if (err) console.log(err);
+		var dbo = db.db("sungun");
+		var data = dbo.collection('tblExcercise').find().toArray(function(err, items){
+			res.send(items);
+		});
+	});
+});
+
+app.get('/api/excerciseLookup', (req, res) => {
+	MongoClient.connect(url, function(err, db) {
+  		if (err) console.log(err);
+		var dbo = db.db("sungun");
+		var data = dbo.collection('tblExcersiseLookup').find().toArray(function(err, items){
+			res.send(items);
 		});
 	});
 });
